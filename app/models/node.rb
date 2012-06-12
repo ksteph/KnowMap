f0e413a812c17654b4fc523c6e11a1b515e85c02
Omar Ali <github@omarali.us>
2012-06-11 19:58:47 -0700

    node edit page is working

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,24 +1,29 @@
 class Node < ActiveRecord::Base
-  attr_accessible :content, :title, :incoming_edges_attributes, :outgoing_edges_attributes
+  attr_accessible :content, :title, :incoming_edges_attributes, :outgoing_edges_attributes, :related_edges_A_attributes, :related_edges_B_attributes
   has_many :graph_membership_nodes
   has_many :graphs, :through => :graph_membership_nodes
   
-  has_many :incoming_edges, :class_name => "Edge", :foreign_key => "node_id_B"
-  has_many :outgoing_edges, :class_name => "Edge", :foreign_key => "node_id_A"
+  has_many :incoming_edges, :class_name => "Edge", :conditions => { :edgetype_id => Edgetype.find_by_name('dependent') }, :foreign_key => "node_id_B"
+  has_many :outgoing_edges, :class_name => "Edge", :conditions => { :edgetype_id => Edgetype.find_by_name('dependent') }, :foreign_key => "node_id_A"
+  has_many :related_edges_A,  :class_name => "Edge", :conditions => { :edgetype_id => Edgetype.find_by_name('related') }, :foreign_key => "node_id_B"
+  has_many :related_edges_B,  :class_name => "Edge", :conditions => { :edgetype_id => Edgetype.find_by_name('related') }, :foreign_key => "node_id_A"
   
-  #has_many :parent_nodes, :through => :incoming_edges, :class_name => "Node", :source => "node_A"
-  #has_many :child_nodes, :through => :outgoing_edges, :class_name => "Node", :source => "node_B"
+  has_many :previous_nodes, :through => :incoming_edges, :class_name => "Node", :source => "node_A"
+  has_many :next_nodes, :through => :outgoing_edges, :class_name => "Node", :source => "node_B"
   
+  accepts_nested_attributes_for :incoming_edges, :reject_if => lambda { |a| a[:node_id_A].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :outgoing_edges, :reject_if => lambda { |a| a[:node_id_B].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :related_edges_A, :reject_if => lambda { |a| a[:node_id_A].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :related_edges_B, :reject_if => lambda { |a| a[:node_id_B].blank? }, :allow_destroy => true
   
-  accepts_nested_attributes_for :incoming_edges, :reject_if => lambda { |a| a[:edge_id].blank? }, :allow_destroy => true
-  
-  def previous_nodes
-    Edge.where(:edgetype_id => Edgetype.find_by_name('dependent'), :node_id_B => self).all.map! { |e| e.node_A }
-  end
-  def next_nodes
-    Edge.where(:edgetype_id => Edgetype.find_by_name('dependent'), :node_id_A => self).all.map! { |e| e.node_B }
-  end
+  #def previous_nodes
+  #  Edge.where(:edgetype_id => Edgetype.find_by_name('dependent'), :node_id_B => self).all.map! { |e| e.node_A }
+  #end
+  #def next_nodes
+  #  Edge.where(:edgetype_id => Edgetype.find_by_name('dependent'), :node_id_A => self).all.map! { |e| e.node_B }
+  #end
   def related_nodes
-    Edge.where(:edgetype_id => Edgetype.find_by_name('related'), :node_id_A => self).all.map! { |e| e.node_B } | Edge.where(:edgetype_id => Edgetype.find_by_name('related'), :node_id_B => self).all.map! { |e| e.node_A }
+    #Edge.where(:edgetype_id => Edgetype.find_by_name('related'), :node_id_A => self).all.map! { |e| e.node_B } | Edge.where(:edgetype_id => Edgetype.find_by_name('related'), :node_id_B => self).all.map! { |e| e.node_A }
+    related_edges_A.map! { |e| e.node_A } | related_edges_B.map! { |e| e.node_B }
   end
 end
