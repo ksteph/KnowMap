522a6c4a8755afa473ae6c093b332157e61ca802
Omar Ali <github@omarali.us>
2012-06-25 12:11:38 -0700

    set jQuery to load from google CDN

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//  require jquery
 //= require jquery_ujs
 //          history_jquery_html5
 //= require_tree .
