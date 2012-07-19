3edc9753c96313eb9a61e821f09b5102b6000c50
Kristin Stephens <ksteph@cs.berkeley.edu>
2012-07-19 15:57:57 -0700

    Code cleaning for map and move constants and styles to map.js and graphs.css

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,19 @@
+var MAP_CONSTANTS = {
+    zoom_scale : 1.1,
+    pan_step : 10,
+
+    node_radius : 8,
+    node_shift_x : 100,
+    node_shift_y : 0,
+
+    edge_style_related : "1,2",
+    edge_style_dependent : "1,0",
+
+    highlight_colors : ["#ff0000","#ff9900","#fff333","#00cc00","#3333ff"],
+    highlight_radius : 11,
+    highlight_opacity : 0.5,
+};
+
+/*
+  highlight colors are from here: http://www.colorschemer.com/schemes/viewscheme.php?id=9159
+ */
