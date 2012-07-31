a4f5a7a1caca69f82a5cd82eea0266c43adf92da
Omar Ali <github@omarali.us>
2012-07-31 13:37:50 -0700

    fixed fetching all nodes in course

--- app/models/course.rb
+++ app/models/course.rb
@@ -15,5 +15,5 @@ class Course < ActiveRecord::Base
   has_many :students, :through => :student_memberships, :class_name => "User", :source => :user
   accepts_nested_attributes_for :student_memberships, :reject_if => lambda { |a| a[:user_id].blank? }, :allow_destroy => true
   
-  has_many :nodes, :finder_sql => proc { "WITH RECURSIVE subgraphs AS ( SELECT subgraph_id FROM graph_membership_graphs WHERE graph_id = #{id} UNION ALL SELECT g.subgraph_id FROM graph_membership_graphs g, subgraphs sg WHERE g.graph_id = sg.subgraph_id) SELECT n.*, ni.row AS row, ni.index AS index FROM nodes n LEFT JOIN node_indices ni ON n.id = ni.node_id WHERE n.id IN (SELECT DISTINCT m.node_id FROM graph_membership_nodes m, subgraphs sg WHERE m.graph_id = sg.subgraph_id OR m.graph_id = #{id}) ORDER BY row, index ASC" }
+  has_many :nodes, :finder_sql => proc { "WITH RECURSIVE subgraphs AS ( SELECT subgraph_id FROM graph_membership_graphs WHERE graph_id = #{graph.id} UNION ALL SELECT g.subgraph_id FROM graph_membership_graphs g, subgraphs sg WHERE g.graph_id = sg.subgraph_id) SELECT n.*, ni.row AS row, ni.index AS index FROM nodes n LEFT JOIN node_indices ni ON n.id = ni.node_id WHERE n.id IN (SELECT DISTINCT m.node_id FROM graph_membership_nodes m, subgraphs sg WHERE m.graph_id = sg.subgraph_id OR m.graph_id = #{graph.id}) ORDER BY row, index ASC" }
 end
