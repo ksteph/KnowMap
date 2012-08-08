0fc9e8d17f90b88e48dc0773edef6410d07d862f
Omar Ali <github@omarali.us>
2012-08-07 18:27:37 -0700

    fix bug where select group's nodes aren't highlighted in map

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -6,7 +6,7 @@ class Graph < ActiveRecord::Base
   has_many :graph_membership_nodes
   has_one :course
   has_many :all_subgraphs, :class_name => "Graph", :finder_sql => proc { "WITH RECURSIVE subgraphs AS ( SELECT subgraph_id FROM graph_membership_graphs WHERE graph_id = #{id} UNION ALL SELECT g.subgraph_id FROM graph_membership_graphs g, subgraphs sg WHERE g.graph_id = sg.subgraph_id) SELECT DISTINCT g.* FROM subgraphs sg JOIN graphs g on g.id = sg.subgraph_id;" }
-  has_many :all_nodes, :class_name => "Node", :finder_sql => proc { "WITH RECURSIVE subgraphs AS ( SELECT subgraph_id FROM graph_membership_graphs WHERE graph_id = #{id} UNION ALL SELECT g.subgraph_id FROM graph_membership_graphs g, subgraphs sg WHERE g.graph_id = sg.subgraph_id) SELECT * FROM nodes WHERE nodes.id IN (SELECT DISTINCT m.node_id FROM graph_membership_nodes m, subgraphs sg WHERE m.graph_id = sg.subgraph_id OR m.graph_id = #{id})" }
+  has_many :all_nodes, :class_name => "Node", :finder_sql => proc { "WITH RECURSIVE subgraphs AS ( SELECT subgraph_id FROM graph_membership_graphs WHERE graph_id = #{id} UNION ALL SELECT g.subgraph_id FROM graph_membership_graphs g, subgraphs sg WHERE g.graph_id = sg.subgraph_id) SELECT * FROM nodes WHERE nodes.id IN (SELECT DISTINCT m.node_id FROM graph_membership_nodes m, subgraphs sg WHERE m.graph_id = sg.subgraph_id UNION ALL SELECT DISTINCT m.node_id FROM graph_membership_nodes m WHERE m.graph_id = #{id})" }
   
   accepts_nested_attributes_for :graph_membership_graphs, :reject_if => lambda { |a| a[:subgraph_id].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :graph_membership_nodes, :reject_if => lambda { |a| a[:node_id].blank? }, :allow_destroy => true
